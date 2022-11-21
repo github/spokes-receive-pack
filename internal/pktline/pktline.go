b98d0723d5dbcf7cbd08ad5c2f1d620b83e796d8
Miguel Ángel Pastor Olivar <migue@github.com>
2022-11-21 09:28:48 +0100

    Packet line abstraction
    This abstraction allows us to represent a packet line which is the basic form of communication in the pack protocol. This new type provides the ability to read the packet line and to process the capabilities using the abstraction introduced before

---
+++ internal/pktline/pktline.go
@@ -0,0 +1,115 @@
+package pktline
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+const (
+	MaxPayload = 65519
+	HeaderSize = 4
+)
+
+var FlushPktline = []byte("0000")
+var HeartbeatPktline = []byte("0004")
+
+type Pktline struct {
+	buf                   [HeaderSize + MaxPayload + 1]byte
+	payloadSize           []byte
+	Payload               []byte
+	CapabilitiesPayload   []byte
+	processedCapabilities bool
+}
+
+func New() *Pktline {
+	pl := Pktline{}
+	pl.Reset()
+	return &pl
+}
+
+func (pl *Pktline) IsFlush() bool {
+	return bytes.Equal(pl.payloadSize, []byte("0000"))
+}
+
+func (pl *Pktline) IsHeartbeat() bool {
+	return bytes.Equal(pl.payloadSize, []byte("0004"))
+}
+
+func (pl *Pktline) Capabilities() (Capabilities, error) {
+	return ParseCapabilities(pl.CapabilitiesPayload)
+}
+
+// Size returns the total size of `pl` (including the length) by
+// parsing `pl.payloadSize`.
+func (pl *Pktline) Size() (int, error) {
+	size, err := strconv.ParseUint(string(pl.payloadSize), 16, 16)
+	if err != nil {
+		return 0, fmt.Errorf("read-header: illformed pktline size: %w", err)
+	}
+
+	if size > HeaderSize+MaxPayload+1 {
+		return 0, fmt.Errorf("read-header: invalid pkt-line length: %d", size)
+	}
+	return int(size), nil
+}
+
+// Reset resets the pktline to read the next pktline
+func (pl *Pktline) Reset() {
+	pl.payloadSize = pl.buf[:4]
+	pl.Payload = pl.buf[4:]
+}
+
+// Read reads the next pktline from `r` into `pl` (resetting `pl`
+// first). If `r` is already at EOF, return `io.EOF`. If EOF is
+// encountered after reading part but not all the pktline, return
+// `io.ErrUnexpectedEOF`.
+func (pl *Pktline) Read(r io.Reader) error {
+	pl.Reset()
+	// Read header
+	if _, err := io.ReadFull(r, pl.payloadSize); err != nil {
+		if err == io.EOF {
+			return io.EOF
+		}
+		return fmt.Errorf("reading pktline size: %w", err)
+	}
+
+	size, err := pl.Size()
+	if err != nil {
+		return err
+	}
+
+	if size <= HeaderSize {
+		// No payload
+		pl.Payload = pl.buf[4:4]
+		return nil
+	}
+
+	// Read payload
+	if _, err := io.ReadFull(r, pl.buf[4:size]); err != nil {
+		if err == io.EOF {
+			err = io.ErrUnexpectedEOF
+		}
+		return fmt.Errorf("reading pktline data: %w", err)
+	}
+
+	pl.Payload = pl.buf[4:size]
+
+	// Capabilities are (optionally) sent along the first packet line
+	if !pl.processedCapabilities {
+		if pl.Payload[len(pl.Payload)-1] == 0 {
+			pl.Payload = pl.Payload[:len(pl.Payload)-1]
+			buffReader := bufio.NewReader(r)
+			caps, err := buffReader.ReadBytes('\n')
+			if err != nil {
+				return fmt.Errorf("reading capabilities: %w", err)
+			}
+			pl.CapabilitiesPayload = caps
+			pl.processedCapabilities = true
+		}
+	}
+
+	return nil
+}
---
+++ internal/pktline/pktline_test.go
@@ -0,0 +1,165 @@
+package pktline_test
+
+import (
+	"errors"
+	"fmt"
+	"github.com/github/spokes-receive-pack/internal/pktline"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"strings"
+	"testing"
+)
+
+type expectedPktline struct {
+	size    int
+	payload string
+}
+
+var expectFlush = expectedPktline{
+	size:    0,
+	payload: "",
+}
+
+func (expected *expectedPktline) CheckEqual(pl *pktline.Pktline) error {
+	size, err := pl.Size()
+	if err != nil {
+		return fmt.Errorf("invalid pktline size: %w", err)
+	}
+	if size != expected.size {
+		return fmt.Errorf("incorrect pktline size: expected %d, got %d", expected.size, size)
+	}
+
+	payload := string(pl.Payload)
+	if payload != expected.payload {
+		return fmt.Errorf(
+			"incorrect pktline payload: expected %q, got %q",
+			expected.payload, payload,
+		)
+	}
+	return nil
+}
+
+func TestRead(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		input    string
+		expected []expectedPktline
+	}{
+		{
+			name:     "nothing",
+			input:    "",
+			expected: nil,
+		},
+		{
+			name:  "flush",
+			input: "0000",
+			expected: []expectedPktline{
+				expectFlush,
+			},
+		},
+		{
+			name:  "short",
+			input: "0002",
+			expected: []expectedPktline{
+				{
+					size:    2,
+					payload: "",
+				},
+			},
+		},
+		{
+			name:  "keepalive",
+			input: "0004",
+			expected: []expectedPktline{
+				{
+					size:    4,
+					payload: "",
+				},
+			},
+		},
+		{
+			name:  "receive-pack-packet-line",
+			input: "006874730d410fcb6603ace96f1dc55ea6196122532d 5a3f6be755bbb7deae50065988cbfa1ffa9ab68a refs/heads/master\n",
+			expected: []expectedPktline{
+				{
+					size:    104,
+					payload: "74730d410fcb6603ace96f1dc55ea6196122532d 5a3f6be755bbb7deae50065988cbfa1ffa9ab68a refs/heads/master\n",
+				},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			pl := pktline.New()
+			r := strings.NewReader(tc.input)
+			for i, expected := range tc.expected {
+				assert.NoError(t, pl.Read(r), "reading pktline")
+				assert.NoErrorf(t, expected.CheckEqual(pl), "pktline %d incorrect", i)
+			}
+
+			err := pl.Read(r)
+			assert.True(t, errors.Is(err, io.EOF), "expected io.EOF after reading all pktlines")
+		})
+	}
+}
+
+func TestParseLineWithCapabilities(t *testing.T) {
+	pl := pktline.New()
+	r := strings.NewReader("00677d1665144a3a975c05f1f43902ddaf084e784dbe 74730d410fcb6603ace96f1dc55ea6196122532d refs/heads/debug\000report-status delete-refs ofs-delta\n")
+
+	err := pl.Read(r)
+	require.NoError(t, err, "reading pktline")
+
+	caps, err := pl.Capabilities()
+	assert.NoError(t, err)
+
+	assert.Equal(t, "report-status", caps.ReportStatus().Name())
+
+	expected := expectedPktline{
+		size:    103,
+		payload: "7d1665144a3a975c05f1f43902ddaf084e784dbe 74730d410fcb6603ace96f1dc55ea6196122532d refs/heads/debug",
+	}
+
+	assert.NoError(t, expected.CheckEqual(pl))
+}
+
+func TestReadErrors(t *testing.T) {
+	for _, tc := range []struct {
+		name          string
+		input         string
+		expectedError string
+	}{
+		{
+			name:          "truncated-size",
+			input:         "01",
+			expectedError: "unexpected EOF",
+		},
+		{
+			name:          "invalid-size",
+			input:         "foob",
+			expectedError: "illformed pktline size",
+		},
+		{
+			name:          "truncated-payload",
+			input:         "fff4" + "2" + "not enough bytes",
+			expectedError: "unexpected EOF",
+		},
+		{
+			name:          "size-too-large",
+			input:         "fff5" + "2" + "these bytes not read",
+			expectedError: "read-header: invalid pkt-line length",
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			pl := pktline.New()
+			r := strings.NewReader(tc.input)
+			err := pl.Read(r)
+			if !strings.Contains(err.Error(), tc.expectedError) {
+				t.Fatal(
+					"expected error '"+tc.expectedError+
+						"' after reading all pktlines; got ", err,
+				)
+			}
+		})
+	}
+}
