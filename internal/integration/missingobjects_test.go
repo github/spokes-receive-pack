9e7d75d27210047544237dfc1d9f79786263882f
Matt Burke <spraints@gmail.com>
2023-04-13 12:39:31 -0400

    Make sure we can still read individual objects

--- internal/integration/missingobjects_test.go
+++ internal/integration/missingobjects_test.go
@@ -4,15 +4,19 @@ package integration
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/github/go-pipe/pipe"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -28,6 +32,8 @@ func TestMissingObjects(t *testing.T) {
 		badPack  = "testdata/missing-objects/bad.pack"
 		infoFile = "testdata/missing-objects/info.json"
 		otherRef = "refs/heads/other"
+
+		quarantineID = "test-quarantine-id"
 	)
 
 	infoJSON, err := os.ReadFile(infoFile)
@@ -48,7 +54,7 @@ func TestMissingObjects(t *testing.T) {
 	srp.Dir = testRepo
 	srp.Env = append(os.Environ(),
 		"GIT_SOCKSTAT_VAR_spokes_quarantine=bool:true",
-		"GIT_SOCKSTAT_VAR_quarantine_id=config-test-quarantine-id")
+		"GIT_SOCKSTAT_VAR_quarantine_id="+quarantineID)
 	srp.Stderr = os.Stderr
 	srpIn, err := srp.StdinPipe()
 	require.NoError(t, err)
@@ -94,4 +100,55 @@ func TestMissingObjects(t *testing.T) {
 		otherRef: "ok",
 	}, refStatus)
 	assert.Equal(t, "unpack ok\n", unpackRes)
+
+	// Now, make sure that we can read all of the objects we just received.
+	quarantineDir := filepath.Join(testRepo, "objects", quarantineID)
+	quarantinePackDir := filepath.Join(quarantineDir, "pack")
+	packs, err := os.ReadDir(quarantinePackDir)
+	require.NoError(t, err)
+	var idxF io.Reader
+	for i := range packs {
+		f := packs[i]
+		fn := f.Name()
+		if strings.HasSuffix(fn, ".idx") {
+			t.Logf("found index file %q", fn)
+			assert.Nil(t, idxF, "expected only one idx file")
+			f, err := os.Open(filepath.Join(quarantinePackDir, fn))
+			require.NoError(t, err)
+			idxF = f
+			defer f.Close()
+		}
+	}
+	require.NotNil(t, idxF, "expected at least one pack index file")
+
+	var checkOut bytes.Buffer
+	p := pipe.New(
+		pipe.WithStdout(&checkOut),
+		pipe.WithStdin(idxF),
+		pipe.WithEnvVars([]pipe.EnvVar{
+			{Key: "GIT_ALTERNATE_OBJECT_DIRECTORIES", Value: filepath.Join(testRepo, "objects")},
+			{Key: "GIT_OBJECT_DIRECTORY", Value: quarantineDir},
+			{Key: "GIT_QUARANTINE_PATH", Value: quarantineDir},
+		}),
+	)
+	p.Add(pipe.Command("git", "--git-dir", testRepo, "show-index"))
+	p.Add(pipe.LinewiseFunction("parse show-index output", func(_ context.Context, _ pipe.Env, line []byte, stdout *bufio.Writer) error {
+		parts := bytes.Split(line, []byte(" "))
+		if len(parts) < 2 {
+			return fmt.Errorf("invalid output from show-index: %s", line)
+		}
+		_, err := fmt.Fprintf(stdout, "%s\n", parts[1])
+		return err
+	}))
+	p.Add(pipe.Command("git", "cat-file", "--batch"))
+	assert.NoError(t, p.Run(ctx))
+	assert.Equal(t, `04ae1b030995df0ae3e91c41d1a065aaae405397 commit 205
+tree 4b825dc642cb6eb9a060e54bf8d69288fbee4904
+parent 8f7da4e89f96103db885fd8919a7dbb245e70d59
+author Hubot <hubot@github.com> 1681399020 +1200
+committer Hubot <hubot@github.com> 1681399020 +1200
+
+commit C
+
+`, checkOut.String())
 }
