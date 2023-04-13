647f08bc7e96b80d2b4b1d17638794d5d32bd9bd
Matt Burke <spraints@gmail.com>
2023-04-13 12:12:04 -0400

    See what happens when we make another, valid, ref update

--- internal/integration/missingobjects_test.go
+++ internal/integration/missingobjects_test.go
@@ -27,6 +27,7 @@ func TestMissingObjects(t *testing.T) {
 		remote   = "testdata/missing-objects/remote.git"
 		badPack  = "testdata/missing-objects/bad.pack"
 		infoFile = "testdata/missing-objects/info.json"
+		otherRef = "refs/heads/other"
 	)
 
 	infoJSON, err := os.ReadFile(infoFile)
@@ -65,9 +66,15 @@ func TestMissingObjects(t *testing.T) {
 		info.Ref: info.OldOID,
 	})
 
+	// Try to update the ref that's already there to commit C (but we won't
+	// push its parent and the remote doesn't have the parent either).
 	require.NoError(t, writePktlinef(srpIn,
 		"%s %s %s\x00report-status report-status-v2 side-band-64k object-format=sha1\n",
 		info.OldOID, info.NewOID, info.Ref))
+	// Try to create another ref with a commit that the remote already has.
+	require.NoError(t, writePktlinef(srpIn,
+		"%040d %s %s",
+		0, info.OldOID, otherRef))
 	_, err = srpIn.Write([]byte("0000"))
 	require.NoError(t, err)
 
@@ -84,6 +91,7 @@ func TestMissingObjects(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, map[string]string{
 		info.Ref: "ng missing necessary objects",
+		otherRef: "ok",
 	}, refStatus)
 	assert.Equal(t, "unpack ok\n", unpackRes)
 }
